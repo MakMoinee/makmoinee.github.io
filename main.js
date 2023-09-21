ed7f46b2afd9f3f7960d1371c8827d51ca70c83b
Mak Moinee <titus.silver.ph@gmail.com>
2023-09-22 00:45:57 +0800

    update main

--- main.js
+++ main.js
@@ -1,14 +1,76 @@
 const startButton = document.getElementById('startButton');
 const stopButton = document.getElementById('stopButton');
+const toggleAudioButton = document.getElementById('toggleAudioButton');
 const recordedVideo = document.getElementById('recordedVideo');
 let mediaRecorder;
 let recordedChunks = [];
+let screenStream;
+let audioStream;
 
 startButton.addEventListener('click', async () => {
     try {
-        const stream = await navigator.mediaDevices.getDisplayMedia({ video: { mediaSource: 'screen' } });
+        startButton.disabled = true;
+        startCountdown(3); // Start a 3-second countdown before recording.
+    } catch (error) {
+        console.error('Error accessing screen sharing:', error);
+    }
+});
 
-        mediaRecorder = new MediaRecorder(stream);
+toggleAudioButton.addEventListener('click', async () => {
+    if (audioStream) {
+        // Disable audio (microphone and system audio).
+        audioStream.getTracks().forEach(track => track.stop());
+        audioStream = null;
+        toggleAudioButton.innerText = 'Enable Audio';
+    } else {
+        // Enable audio (microphone and system audio).
+        try {
+            audioStream = await navigator.mediaDevices.getUserMedia({ audio: true });
+            toggleAudioButton.innerText = 'Disable Audio';
+        } catch (error) {
+            console.error('Error accessing audio:', error);
+        }
+    }
+});
+
+function startCountdown(seconds) {
+    let counter = seconds;
+
+    const countdownInterval = setInterval(() => {
+        if (counter > 0) {
+            console.log(`Recording will start in ${counter} seconds...`);
+            counter--;
+        } else {
+            clearInterval(countdownInterval);
+            startRecording();
+        }
+    }, 1000);
+}
+
+async function startRecording() {
+    try {
+        const screenOptions = { video: { mediaSource: 'screen' } };
+
+        // Include audio streams if available.
+        if (audioStream) {
+            screenOptions.audio = {
+                mandatory: {
+                    chromeMediaSource: 'desktop',
+                    chromeMediaSourceId: screenStream.id,
+                },
+            };
+        }
+
+        screenStream = await navigator.mediaDevices.getDisplayMedia(screenOptions);
+
+        // Merge screen and audio streams into one stream.
+        const recordedStream = new MediaStream();
+        screenStream.getTracks().forEach(track => recordedStream.addTrack(track));
+        if (audioStream) {
+            audioStream.getTracks().forEach(track => recordedStream.addTrack(track));
+        }
+
+        mediaRecorder = new MediaRecorder(recordedStream);
         mediaRecorder.ondataavailable = (event) => {
             if (event.data.size > 0) {
                 recordedChunks.push(event.data);
@@ -20,30 +82,29 @@ startButton.addEventListener('click', async () => {
             const videoURL = window.URL.createObjectURL(blob);
             recordedVideo.src = videoURL;
 
-            // Create a download link
+            // Create a download link.
             const a = document.createElement('a');
             a.style.display = 'none';
             a.href = videoURL;
             a.download = 'recorded-video.webm';
             document.body.appendChild(a);
 
-            // Trigger the download
+            // Trigger the download.
             a.click();
 
-            // Clean up
+            // Clean up.
             window.URL.revokeObjectURL(videoURL);
         };
 
         mediaRecorder.start();
-        startButton.disabled = true;
         stopButton.disabled = false;
     } catch (error) {
         console.error('Error accessing screen sharing:', error);
     }
-});
+}
 
 stopButton.addEventListener('click', () => {
-    if (mediaRecorder.state === 'recording') {
+    if (mediaRecorder && mediaRecorder.state === 'recording') {
         mediaRecorder.stop();
         startButton.disabled = false;
         stopButton.disabled = true;
